3e6bdd6c65a8fb90334dfa6ab67aef4cafaddc6f
miyagee <jieli280495@gmail.com>
2016-02-10 11:31:03 +0100

    add userclass

--- app/src/main/java/com/now/live/livenow/User.java
+++ app/src/main/java/com/now/live/livenow/User.java
@@ -7,10 +7,45 @@ public class User {
 
     private String email;
     private String name;
-    //private String
+    private String picture;
+    private String password;
+    private int discoverRange;
 
-    public User(String email, String name){
 
+    public User(String email, String name, String password, String picture, int discoverRange){
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.password = password;
+        this.discoverRange = discoverRange;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPicture(String picture){
+        this.picture = picture;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setDiscoverRange(int discoverRange){
+        this.discoverRange = discoverRange;
     }
 
 }
