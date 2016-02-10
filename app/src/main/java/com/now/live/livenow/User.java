652a270e4222dc6e0529f2b60cbcc30260c1a51c
miyagee <jieli280495@gmail.com>
2016-02-10 10:23:00 +0100

    add internet permission

---
+++ app/src/main/java/com/now/live/livenow/User.java
@@ -0,0 +1,16 @@
+package com.now.live.livenow;
+
+/**
+ * Created by jieli on 09.02.16.
+ */
+public class User {
+
+    private String email;
+    private String name;
+    //private String
+
+    public User(String email, String name){
+
+    }
+
+}
